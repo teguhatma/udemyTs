8378b8aac3eb58c982a831a9e6be8141b8c46722
teguhatma <teguh.yudha@finaccel.co>
2022-08-01 20:33:12 +0700

    42. Functions in Interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+interface Reportable {
   summary(): string;
 }
 
@@ -14,8 +11,8 @@ const oldCivic = {
   },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.summary()}`);
+const printSummary = (item: Reportable): void => {
+  console.log(`Name: ${item.summary()}`);
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
