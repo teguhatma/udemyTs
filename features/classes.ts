59f3a4ff1557aa2057406d4f702ca0463e094ec1
teguhatma <teguh.yudha@finaccel.co>
2022-08-01 23:30:00 +0700

    48. Fields in Classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,23 +1,24 @@
 class Vehicle {
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log('beep');
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.honk();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log('vroom');
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log('vroom');
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const kar = new Car();
-kar.startDrivingProcess();
-kar.honk();
+// const kar = new Car();
+// kar.startDrivingProcess();
