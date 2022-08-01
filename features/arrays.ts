64429bd4d515d610c63c811fa311d55e5d46105d
teguhatma <teguh.yudha@finaccel.co>
2022-08-01 17:36:02 +0700

    32. Why Typed Arrays?

--- features/arrays.ts
+++ features/arrays.ts
@@ -2,3 +2,15 @@ const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string) => {
+  return car.toUpperCase();
+});
