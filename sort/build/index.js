a8026f339b45b7c94b432423e52c4d32cd28480b
teguhatma <teguh.yudha@finaccel.co>
2022-08-03 21:51:46 +0700

    78. Concurrent Compilation and Execution

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,5 +1,6 @@
 "use strict";
-console.log('Hi, there!');
+console.log('Hi, guys!');
 const logSomething = () => {
     console.log('something');
 };
+logSomething();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,7 @@
-console.log('Hi, there!');
+console.log('Hi, guys!');
 
 const logSomething = () => {
   console.log('something');
 };
+
+logSomething();
