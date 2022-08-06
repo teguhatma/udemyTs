9a5499ec237b6f797e2929e57e98938fbf236bbd
teguhatma <teguh.yudha@finaccel.co>
2022-08-07 01:18:15 +0700

    89. Interface Definition

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0, -10]);
+const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0, -101]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0, -10]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0, -101]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
