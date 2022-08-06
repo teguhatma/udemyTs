824996644be204583f32f8a12ab543a71cb1d9d8
teguhatma <teguh.yudha@finaccel.co>
2022-08-07 00:20:00 +0700

    84. Type Guards

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -6,14 +6,20 @@ class Sorter {
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        // All of this only works if collection is number[]
         // if collection is an array of numbers
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
-
+        // Only going to work if collection is a string
         // if collection is a string, do this logic instead
+        if (typeof this.collection === 'string') {
+          this.collection;
+        }
       }
     }
   }
