19983c52eed4e5983a87cfc71cf0e047a335d0e6
teguhatma <teguh.yudha@finaccel.co>
2022-08-07 00:06:29 +0700

    83. Typescript is Really Smart

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,16 +1,19 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+        // if collection is an array of numbers
         if (this.collection[j] > this.collection[j + 1]) {
           const leftHand = this.collection[j];
           this.collection[j] = this.collection[j + 1];
           this.collection[j + 1] = leftHand;
         }
+
+        // if collection is a string, do this logic instead
       }
     }
   }
