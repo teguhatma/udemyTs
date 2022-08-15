79e25b9d6c9f3b9f996ed082fb203eaeb4d517c1
teguhatma <teguh.yudha@finaccel.co>
2022-08-15 22:15:58 +0700

    98. Solving All Issues with Abstract Classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,19 @@
-import { Sorter } from './Sorter';
+// import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkeList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, -101]);
 numbersCollection.sort();
+console.log(numbersCollection.data);
 // const sorter = new Sorter(charactersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('teguh');
+const charactersCollection = new CharactersCollection('teguh');
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
 // const sorter = new Sorter(charactersCollection);
 // sorter.sort();
 // console.log(charactersCollection.data);
@@ -20,6 +24,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
