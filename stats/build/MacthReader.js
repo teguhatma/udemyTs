e389bacb3878559f750564b01f43f38403dfe2ca
teguhatma <teguhatmayudha@gmail.com>
2022-08-17 20:00:18 +0700

    121. Interface-Based Approach

---
+++ stats/build/MacthReader.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MatchReader = void 0;
+class MatchReader {
+    constructor(reader) {
+        this.reader = reader;
+    }
+}
+exports.MatchReader = MatchReader;
