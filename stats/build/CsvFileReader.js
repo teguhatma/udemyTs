f8baab4a6e014f3aaff5847934d225c2f369e19a
teguhatma <teguhatmayudha@gmail.com>
2022-08-17 19:05:10 +0700

    117. Creating Abstract Classes

--- stats/build/CsvFileReader.js
+++ stats/build/CsvFileReader.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 const fs_1 = __importDefault(require("fs"));
-const utils_1 = require("./utils");
 class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
@@ -21,17 +20,6 @@ class CsvFileReader {
             return row.split(',');
         })
             .map(this.mapRow);
-    }
-    mapRow(row) {
-        return [
-            (0, utils_1.dateStringToDate)(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5],
-            row[6],
-        ];
     }
 }
 exports.CsvFileReader = CsvFileReader;
