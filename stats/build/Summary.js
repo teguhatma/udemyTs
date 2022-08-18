7bcce64297896d79acfd81e9679895af82adfabf
teguhatma <teguhatmayudha@gmail.com>
2022-08-18 21:28:16 +0700

    129. A Composition-Based Approach

---
+++ stats/build/Summary.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+class Summary {
+    constructor(analyzer, outputTarget) {
+        this.analyzer = analyzer;
+    }
+}
+exports.Summary = Summary;
