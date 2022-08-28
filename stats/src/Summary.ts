07ab1f90052f8ee5d3d50ac7e568273a58bdcfc2
teguhatma <teguhatmayudha@gmail.com>
2022-08-28 17:18:48 +0700

    131. Building the Reporter

--- stats/src/Summary.ts
+++ stats/src/Summary.ts
@@ -9,5 +9,10 @@ export interface OutputTarget {
 }
 
 export class Summary {
-  constructor(public analyzer: Analyzer, outputTarget: OutputTarget) {}
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPritReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
