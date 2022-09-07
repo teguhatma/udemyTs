c6027c64ed0972a9f45bfd9e14125fed2449bfd1
teguhatma <teguhatmayudha@gmail.com>
2022-09-07 12:57:29 +0700

    146. Optional Interface Properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,8 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Teguh', age: 20 });
+
+user.set({ name: 'Teguh Atma Yudha' });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
