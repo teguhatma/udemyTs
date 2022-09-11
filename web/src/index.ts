f0d343e439ac95395d67a6427d9b3b71caad51b2
teguhatma <teguhatmayudha@gmail.com>
2022-09-11 22:23:33 +0700

    173. Passthrough Methods

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,16 +1,6 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Teguh Atma', age: 21 });
-
-// A quick remindeer on accessors
-
-class Person {
-  constructor(public firstName: string, public lastName: string) {}
-
-  get fullName(): string {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-
-const person = new Person('Teguh', 'Atma');
-console.log(person.fullName);
+user.on('change', () => {
+  console.log('user was changed');
+});
