924a38fe3b267a7b781a37ea3abca2a7a85755fd
teguhatma <teguhatmayudha@gmail.com>
2022-09-11 23:33:34 +0700

    186. Parsing User JSON

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,7 @@
-import { User } from './models/User';
+import { Collection } from './models/Collection';
 
-const user = User.buildUser({ id: 1 });
-
-user.on('change', () => {
-  console.log(user);
+const collection = new Collection('http://localhost:3000/users');
+collection.on('change', () => {
+  console.log(collection);
 });
-
-user.fetch();
-
-console.log(user.isAdminUser());
+collection.fetch();
--- web/src/models/Collection.ts
+++ web/src/models/Collection.ts
@@ -1,9 +1,12 @@
+import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import { User } from './User';
+import { User, UserProps } from './User';
 
 export class Collection {
   models: User[] = [];
   events: Eventing = new Eventing();
+
+  constructor(public rootUrl: string) {}
 
   get on() {
     return this.events.on;
@@ -11,5 +14,16 @@ export class Collection {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      response.data.forEach((value: UserProps) => {
+        const user = User.buildUser(value);
+        this.models.push(user);
+      });
+    });
+
+    this.trigger('change');
   }
 }
