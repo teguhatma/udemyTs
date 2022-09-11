d50ea60edc70d58377f90b0e0511dc62f1efdb83
teguhatma <teguhatmayudha@gmail.com>
2022-09-11 22:32:58 +0700

    174. A Context Issue

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,10 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Teguh Atma', age: 21 });
+console.log(user.get('name'));
+
 user.on('change', () => {
-  console.log('user was changed');
+  console.log('User was changed');
 });
+
+user.trigger('change');
