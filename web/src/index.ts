fbeff3d8c3bbb7c524165afa67c4cf2d507f153e
teguhatma <teguhatmayudha@gmail.com>
2022-09-11 20:34:00 +0700

    156. Saving User Data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,10 @@
 import { User } from './models/User';
 
+// Put
 const user = new User({ id: 1 });
+user.set({ name: 'Teguh', age: 20 });
+user.save();
 
-user.fetch();
-
-setTimeout(() => {
-  console.log(user);
-}, 4000);
+// Post
+const newUser = new User({ name: 'Teguh Atma', age: 21 });
+newUser.save();
