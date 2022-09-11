71781d84599cff7ddd00460000fefcf054fa1755
teguhatma <teguhatmayudha@gmail.com>
2022-09-11 19:52:22 +0700

    150. Dynamic Array Creation

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Teguh', age: 20 });
 
-user.set({ name: 'Teguh Atma Yudha' });
+user.on('change', () => {});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+console.log(user);
