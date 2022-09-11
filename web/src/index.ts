dc7d56834332f848c8f9e652c9147c5738b7912e
teguhatma <teguhatmayudha@gmail.com>
2022-09-11 22:37:43 +0700

    175. Setting Data While Triggering

--- web/src/index.ts
+++ web/src/index.ts
@@ -4,7 +4,7 @@ const user = new User({ name: 'Teguh Atma', age: 21 });
 console.log(user.get('name'));
 
 user.on('change', () => {
-  console.log('User was changed');
+  console.log('User was changed, Wow!');
 });
 
-user.trigger('change');
+user.set({ name: 'Teguh Atma Yudha' });
