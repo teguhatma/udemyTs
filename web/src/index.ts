71f4c6b0d5541de7e933d56bc8bfe5f8cc8f3178
teguhatma <teguhatmayudha@gmail.com>
2022-09-11 20:24:17 +0700

    155. Successfully Fetching Model Data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,3 +1,9 @@
-import axios from 'axios';
+import { User } from './models/User';
 
-axios.post('http://localhost:3000/users');
+const user = new User({ id: 1 });
+
+user.fetch();
+
+setTimeout(() => {
+  console.log(user);
+}, 4000);
