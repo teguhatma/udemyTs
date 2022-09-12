6e2e4f690d14e25c871afae5bbfd95363dc354e0
teguhatma <teguhatmayudha@gmail.com>
2022-09-13 01:13:19 +0700

    195. Adding Model Properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,6 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'Teguh Atma Yudha', age: 20 });
+const userForm = new UserForm(document.getElementById('root')!, user);
 userForm.render();
