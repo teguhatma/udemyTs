747012e521ddebd702553e1efbfa7ab45594e117
teguhatma <teguhatmayudha@gmail.com>
2022-09-13 02:22:27 +0700

    209. View Nesting

--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -1,5 +1,7 @@
 import { View } from './View';
 import { User, UserProps } from '../models/User';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
 
 export class UserEdit extends View<User, UserProps> {
   regionsMap(): { [key: string]: string } {
@@ -9,12 +11,17 @@ export class UserEdit extends View<User, UserProps> {
     };
   }
 
+  onRender(): void {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
+  }
+
   template(): string {
     return `
-        <div>
-            <div class="user-show"></div>
-            <div class="user-form"></div>
-        </div>
+      <div>
+          <div class="user-show"></div>
+          <div class="user-form"></div>
+      </div>
     `;
   }
 }
