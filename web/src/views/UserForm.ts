3ef43ae3f65253c6509e7a1a3b4c9b9fdc901019
teguhatma <teguhatmayudha@gmail.com>
2022-09-12 11:07:47 +0700

    190. Building the UserForm

---
+++ web/src/views/UserForm.ts
@@ -0,0 +1,12 @@
+export class UseForm {
+  parent: Element;
+
+  template(): string {
+    return `
+        <div>
+            <h1>User Form</h1>
+            <input />
+        </div>
+        `;
+  }
+}
