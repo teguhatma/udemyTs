1b462142e99fea8052e3f4a8cac5d1f65a17a856
teguhatma <teguhatmayudha@gmail.com>
2022-09-13 02:17:34 +0700

    208. Testing Region Mapping

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,4 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'Teguh Atma Yudha', age: 20 });
@@ -6,8 +6,9 @@ const user = User.buildUser({ name: 'Teguh Atma Yudha', age: 20 });
 const root = document.getElementById('root');
 // const userForm = new UserForm(document.getElementById('root')!, user);
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found');
 }
